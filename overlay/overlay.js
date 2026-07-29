19327b523027dd39f0d78f25a6bac96c5649aa76
appeli-koodaa <appelbergeri@gmail.com>
2026-07-29 12:35:53 +0300

    v0.1 HUD prototype

---
+++ overlay/overlay.js
@@ -0,0 +1,14 @@
+function updateClock() {
+
+    const now = new Date();
+
+    const hours = String(now.getHours()).padStart(2, "0");
+    const minutes = String(now.getMinutes()).padStart(2, "0");
+
+    document.getElementById("clock").textContent =
+        `${hours}:${minutes}`;
+}
+
+updateClock();
+
+setInterval(updateClock, 1000);
