24d523a170a748b3fed515ae773bc58dc29546b4
appeli-koodaa <appelbergeri@gmail.com>
2026-07-29 16:33:20 +0300

    Add Firebase authentication support

--- shared/firebase.js
+++ shared/firebase.js
@@ -1,7 +1,10 @@
 // Firebase Appelin FishingHUD
 // shared/firebase.js
 
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+
+
 import {
     getDatabase,
     ref,
@@ -10,24 +13,64 @@ import {
     onValue
 } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-database.js";
 
+
+import {
+    getAuth,
+    onAuthStateChanged,
+    signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+
+
+
 const firebaseConfig = {
+
     apiKey: "AIzaSyC2la2tsFUZ1adgK2QfcgjZoF4W0fgAyBY",
+
     authDomain: "appelin-fishinghud.firebaseapp.com",
-    databaseURL: "https://appelin-fishinghud-default-rtdb.europe-west1.firebasedatabase.app",
+
+    databaseURL:
+        "https://appelin-fishinghud-default-rtdb.europe-west1.firebasedatabase.app",
+
     projectId: "appelin-fishinghud",
-    storageBucket: "appelin-fishinghud.firebasestorage.app",
+
+    storageBucket:
+        "appelin-fishinghud.firebasestorage.app",
+
     messagingSenderId: "594403353007",
-    appId: "1:594403353007:web:57fc6f24a7b28826abcf5a",
-    measurementId: "G-BZ7KDTTX6C"
+
+    appId:
+        "1:594403353007:web:57fc6f24a7b28826abcf5a",
+
+    measurementId:
+        "G-BZ7KDTTX6C"
+
 };
+
+
 
 const app = initializeApp(firebaseConfig);
 
+
+
 export const db = getDatabase(app);
 
+
+export const auth = getAuth(app);
+
+
+
 export {
+
     ref,
+
     set,
+
     get,
-    onValue
+
+    onValue,
+
+    onAuthStateChanged,
+
+    signInWithEmailAndPassword
+
 };
